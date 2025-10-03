1c1d37c21a1aba31c706a0d290708768e063e1b6
Boneless <bonelessmail0101@gmail.com>
2025-10-02 18:52:46 -0700

    renderer update v0.0000001

---
+++ src/main/java/com/boneless/util/Uno2DEngine.java
@@ -0,0 +1,6 @@
+package com.boneless.util;
+
+import javax.swing.*;
+
+public class Uno2DEngine extends JPanel {
+}
--- src/main/java/com/boneless/util/Uno3DEngine.java
+++ src/main/java/com/boneless/util/Uno3DEngine.java
@@ -3,6 +3,15 @@ package com.boneless.util;
 import javax.swing.*;
 
 public class Uno3DEngine extends JPanel {
+
+    static class Vec3 {
+        double x, y, z;
+        Vec3(double x, double y, double z) {this.x = x; this.y = y; this.z = z; }
+    }
+
+    private Vec3 camPos = new Vec3(0, 0, 0);
+    private Vec3 camRot = new Vec3(0, 0, 0);
+
     public Uno3DEngine() {
 
     }

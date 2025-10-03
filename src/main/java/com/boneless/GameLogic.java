0777486e35f43e2a2181eb9c33f99ec59e0beebe
dantejones <dante.ljubic@gmail.com>
2025-10-02 19:48:49 -0700

    Color and rank fix 2?

--- src/main/java/com/boneless/GameLogic.java
+++ src/main/java/com/boneless/GameLogic.java
@@ -12,11 +12,11 @@ public class GameLogic {
             this.value = value;
         }
 
-        public int getValue(){
+        public int getColor(){
             return value;
         }
     }
-    enum Rank  { ZERO(0), ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), SKIP(10), REVERSE(11), DRAW_TWO(12), WILD(13), WILD_DRAW_FOUR(14)
+    enum Rank  { ZERO(0), ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), SKIP(10), REVERSE(11), DRAW_TWO(12), WILD(13), WILD_DRAW_FOUR(14);
     private final int value;
 
         Rank(int value){
@@ -42,11 +42,6 @@ public class GameLogic {
             hands.add(new ArrayList<>());
         }
     }
-
-    public getColor {}{
-
-    }
-
 
 }
 
